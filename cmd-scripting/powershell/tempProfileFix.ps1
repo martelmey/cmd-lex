791c74517ebd27f2781649aeb06e58a835c90c4e
lattitude-cgi <martel.meyers@cgi.com>
2021-02-09 16:14:30 -0800

    windows rds temp profile fix

---
+++ cmd-scripting/powershell/tempProfileFix.ps1
@@ -0,0 +1,4 @@
+Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\
+# Detect .bak entries
+# Find corresponding, non-.bak entry, rename w/ .tmp
+# Remove .bak from valid entries
